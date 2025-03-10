d4a38898a94466015d1fafe14f994716231f63a4
xannykov <sema_zemlyakov@mail.ru>
2025-03-10 16:58:08 +0300

    Refactor code. Add saving values from settings forms.

--- scripts.js
+++ scripts.js
@@ -1,20 +1,26 @@
 function updateRangeValue(value){
+    
     const rangeValue = document.getElementById("range-value");
     rangeValue.textContent = value;
+
     const rangeInput = document.getElementById("range-slider");
+
     const percentage = ((value - rangeInput.min) / (rangeInput.max - rangeInput.min)) * 100;
     rangeValue.style.left = `calc(${percentage}%)`;
 }
 
+
+
 function toggleMode(){
+
     let modeTitle = document.getElementById("mode-title");
     let rulesContent = document.getElementById("rules-content");
 
     if(modeTitle.textContent === "HELP"){
         modeTitle.textContent = "OUTPUT";
         rulesContent.innerHTML = `
-                <p class="output-description">Your generated password:</p>
-                <div class="output-box">********</div>`;
+                <p class="output__description">Your generated password:</p>
+                <div class="output__box">********</div>`;
     }else{
         modeTitle.textContent = "HELP";
         rulesContent.innerHTML = `
@@ -27,3 +33,44 @@ function toggleMode(){
                 </ol>`;
     }
 }
+
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    
+    const generateButton = document.querySelector(".settings__button");
+    const rangeInput = document.querySelector("#range-slider");
+    const keywordInput = document.querySelector(".settings__input-text");
+    const checkboxes = document.querySelectorAll(".settings__input-checkbox");
+
+
+    generateButton.addEventListener("click", function () {
+        const passwordLength = rangeInput.value;
+        const keyword = keywordInput.value.trim();
+        
+
+        if (!keyword) {
+            alert("Enter the keyword!");
+            return;
+        }
+
+
+        const selectedOptions = {};
+        checkboxes.forEach((checkbox) => {
+            selectedOptions[checkbox.id] = checkbox.checked;
+        });
+
+
+        const userSettings = {
+            passwordLength: passwordLength,
+            keyword: keyword,
+            options: selectedOptions,
+        };
+
+
+        localStorage.setItem("userSettings", JSON.stringify(userSettings));
+
+        console.log("Выбранные настройки:", userSettings);
+
+    });
+});
