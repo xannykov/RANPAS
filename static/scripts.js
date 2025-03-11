a5ab5a93cab42a8250ca67af073743037f67a530
xannykov <sema_zemlyakov@mail.ru>
2025-03-11 18:54:30 +0300

    feat (ui): Added a "copy" button. fix (ui): The button for switching the "output" and "help" windows has been fixed. refactor (ui): Removed uppercase from the keyword form.

--- static/scripts.js
+++ static/scripts.js
@@ -15,40 +15,64 @@ function updateRangeValue(value) {
 
 
 
-function toggleMode(){
-
+function toggleMode() {
     let modeTitle = document.getElementById("mode-title");
     let rulesContent = document.getElementById("rules-content");
 
-    if(modeTitle.textContent === "HELP"){
+    let currentMode = modeTitle.textContent.trim(); 
+
+    if (currentMode === "HELP") {
         modeTitle.textContent = "OUTPUT";
         rulesContent.innerHTML = `
-                <p class="output__description">Your generated password:</p>
-                <div class="output__box">********</div>`;
-    }else{
+            <p class="output__description">Your generated password:</p>
+            <div class="output__box">
+                <div class="output__container-output-word" id="output-word">
+                    ExamplePassword1234!
+                </div>
+                <div class="output__container-copy-button">
+                    <button type="submit" class="output__copy-button" id="copy-button" onclick="copyFunc()">COPY</button>
+                </div>
+            </div>`;
+    } else {
         modeTitle.textContent = "HELP";
         rulesContent.innerHTML = `
-                <p>Rules for creating a good password:</p>
-                <ol>
-                    <li>The password must consist of characters, numbers, and letters.</li>
-                    <li>The more characters in the password, the better.</li>
-                    <li>The password should not contain the name and date of birth.</li>
-                    <li>Do not use your personal information (full name, date of birth) as a keyword.</li>
-                </ol>`;
+            <p>Rules for creating a good password:</p>
+            <ol>
+                <li>The password must consist of characters, numbers, and letters.</li>
+                <li>The more characters in the password, the better.</li>
+                <li>The password should not contain the name and date of birth.</li>
+                <li>Do not use your personal information (full name, date of birth) as a keyword.</li>
+            </ol>`;
     }
 }
 
 
 
-document.addEventListener("DOMContentLoaded", function () {
-    
-    document.getElementById("password-form").addEventListener("submit", function (event) {
-        event.preventDefault();
+function copyFunc(){
+    const outputWord = document.getElementById("output-word");
+    const text = outputWord.textContent || outputWord.innerText;
 
-        const formData = new FormData(this);
-        const jsonData = Object.fromEntries(formData.entries());
-
-        console.log("Отправляемые данные:", jsonData);
-
+    navigator.clipboard.writeText(text)
+    .then(() =>{
+    })
+    .catch(err => {
+        alert("Ошибка копирования!");
     });
-});
+}
+
+
+
+// document.addEventListener("DOMContentLoaded", function () {
+    
+//     document.getElementById("password-form").addEventListener("submit", function (event) {
+//         event.preventDefault();
+
+//         toggleMode();
+        
+//         const formData = new FormData(this);
+//         const jsonData = Object.fromEntries(formData.entries());
+
+//         console.log("Отправляемые данные:", jsonData);
+
+//     });
+// });
